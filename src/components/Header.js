7c6a54dabd39e7bab9e541bb82f70cc1b71c9a56
amakadiya98 <amakadiya98@gmail.com>
2023-07-24 11:40:37 +0530

    responsive deisgn changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import {  React, useState} from 'react';
 import '../assets/css/header.css';
 import { BsSearch } from 'react-icons/bs'
 import notificationIcon from '../assets/images/notification.svg'
 import Avatar from 'react-avatar';
 import profileImg from '../assets/images/Ellipse 179.svg'
 import { Dropdown } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return (
         <div className='header-main'>
             <div className="search-part">
@@ -15,7 +19,8 @@ const Header = () => {
             </div>
             <div className="profile-part">
                 <div className="notification-block">
-                    <img src={notificationIcon} alt="notification-icon" width='20px'/>
+                    <img src={notificationIcon} alt="notification-icon" width='20px' onClick={handleShow} style={{ cursor: 'pointer'}}/>
+
                     <div className="noti-no">4</div>
                 </div>
                 <div className="profile">
@@ -30,6 +35,36 @@ const Header = () => {
                     </Dropdown>
                 </div>
             </div>
+            <Modal show={show} onHide={handleClose} centered  size="lg">
+        <Modal.Header closeButton className='justify-content-start border-0'>
+          <Modal.Title className='text-center'>Notifications</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='body-content'>
+            <div className='notification-card'>
+                <h6>Jack Williams</h6>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p className='ms-2'>5:45 PM, Feb 24</p>
+            </div>
+            <div className='notification-card'>
+                <h6>Jack Williams</h6>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p className='ms-2'>5:45 PM, Feb 24</p>
+            </div>
+            <div className='notification-card'>
+                <h6>Jack Williams</h6>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p className='ms-2'>5:45 PM, Feb 24</p>
+            </div>
+            <div className='notification-card'>
+                <h6>Jack Williams</h6>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p className='ms-2'>5:45 PM, Feb 24</p>
+            </div>
+          </div>
+        </Modal.Body>
+      
+      </Modal>
         </div>
     )
 }
